c61b0c5d5fae142f2d341aae8b1d79f8a2417ae2
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-03-11 19:45:04 +0100

    Rad na izborniku

---
+++ Frontend/src/pages/Pocetna.jsx
@@ -0,0 +1,14 @@
+import Container from 'react-bootstrap/Container'
+
+export default function Pocetna(){
+
+    return(
+        <>
+        <Container>
+            Dobro došli na Djelatnici aplikaciju
+        </Container>
+        </>
+    );
+
+
+}
