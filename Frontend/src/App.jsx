4b41b68486addb999293b2f3d79c45aff75a3bc4
Antonio Miloloža <128092036+Mili666999@users.noreply.github.com>
2024-03-11 18:57:32 +0100

    ...

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,9 @@
-import { Route, Routes } from "react-router-dom"
-import Pocetna from "./pages/Pocetna"
-import { RoutesNames } from "./constants"
-import NavBar from "./components/NavBar"
-
 
 function App() {
+
   return (
     <>
-    <NavBar />
-        <Routes>
-        <>
-        <Route path = {RoutesNames.HOME} element = {<Pocetna/>}/>
-        </>
-      </Routes>
+      Hello World!
     </>
   )
 }
